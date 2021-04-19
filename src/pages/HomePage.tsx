e8acba1c25c1cbd828c41aca187dacfb1b3b710e
Ming <minghaochen912@gmail.com>
2021-04-19 11:29:55 -0700

    all packages table now has formatted values

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Col, Row, Divider, Button, Icon, Table } from "antd";
 import React from "react";
 import { RouteProps, RouteChildrenProps } from "react-router";
-import { IPackage } from "../commons/interfaces";
+import { IPackage, IResponseUser } from "../commons/interfaces";
 import { GlobalState, IGlobalState } from "../providers";
 import { MetaInfoCard } from "../components/PSMetaInfoCard";
 import { IntegrationsInfoCard } from "../components/PSIntegrationsInfoCard";
@@ -15,6 +15,7 @@ import {
 } from "../components/UIFragments";
 import { Link } from "react-router-dom";
 import Column from "antd/lib/table/Column";
+import { create } from "lodash";
 
 export class Homepage extends React.Component<RouteChildrenProps> {
   render() {
@@ -115,6 +116,57 @@ export class HomepageInternal extends React.Component<
   };
 
   render() {
+    let pkgs = this.state.displayedPackages;
+    console.log(pkgs);
+
+    // Set up table for ALL PACKAGES
+    let tableColumns = [
+      // Package Name
+      {
+        title: "Package Name",
+        dataIndex: "slug",
+        key: "slug",
+        render: (pkgName: string) => // Render the package name as a link (following above format)
+          <Link to={`/${this.props.context.selectedPackageSet!.slug}/${pkgName}`}>
+            {pkgName}
+          </Link>
+      },
+      // Creator
+      {
+        title: "Created By",
+        dataIndex: "created_by",
+        key: "created_by",
+        render: (createdByObj: IResponseUser) => // Format created by name as Last, First
+          <>{createdByObj.last_name + ", " + createdByObj.first_name}</>
+      },
+      // Last Update
+      {
+        title: "Last Updated",
+        dataIndex: "updated_at",
+        key: "updated_at",
+        render: (lastUpdate: string) => // Format date
+          <>{dateFormatter(new Date(lastUpdate))}</>
+      }
+    ];
+
+    // Formats date in ISO format
+    function dateFormatter(date: Date): string {
+      let dateTimeString = "";
+
+      // Date portion
+      let isoStr = date.toISOString();
+      dateTimeString += isoStr.substring(0, isoStr.lastIndexOf('T'));
+
+      dateTimeString += " ";
+
+      // Time portion
+      let timeStr = date.toTimeString();
+      dateTimeString += timeStr.split(' ')[0];
+
+      return dateTimeString;
+    }
+
+    // Render and return
     return (
       <>
         {this.props.context.selectedPackageSet ? (
@@ -158,10 +210,10 @@ export class HomepageInternal extends React.Component<
 
                   <Divider />
                   <h2>All Packages</h2>
-                  <p>TODO</p>
-                  {/* <Table dataSource={this.state.displayedPackages}>
+                  {/* <p>TODO</p> */}
+                  {<Table dataSource={this.state.displayedPackages} columns={tableColumns}>
                     <Column />
-                  </Table> */}
+                  </Table>}
                 </>
               ) : (
                 <h2>No Packages are found.</h2>
